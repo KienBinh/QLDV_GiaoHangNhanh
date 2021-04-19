4ff8f3ef368b24142f2f726c2c34617c93d11d24
QHung30Code <LQHBackup01@gmail.com>
2021-04-19 18:09:43 +0700

    Commit All

--- sqlDatabase/ConnectionDB.cs
+++ sqlDatabase/ConnectionDB.cs
@@ -9,5 +9,6 @@
         // Hello world
         public int MyProperty { get; set; }
         public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; } //Commit All
     }
 }
